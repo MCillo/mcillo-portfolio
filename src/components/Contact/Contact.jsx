d9a3db582bc6249dff4bb0c7696c0cc080144b63
MCillo <117599499+MCillo@users.noreply.github.com>
2024-02-21 06:41:22 -0500

    Create Contact.jsx
    created Contact.jsx

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:michael.cillo@me.com">michael.cillo@me.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/michael-cillo">linkedin.com/michael-cillo</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/mcillo">github.com/MCillo</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
