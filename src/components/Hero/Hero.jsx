0d72cd6c9b4a787f7cb20434d8258cd551072e3b
Michael Cillo <117599499+MCillo@users.noreply.github.com>
2024-02-26 19:38:46 -0500

    Update Hero.jsx
    updated Hero about text
    updated Hero image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,19 +5,29 @@ import styles from './Hero.module.css';
 import { getImageUrl } from '../../utils';
 
 export const Hero = () => {
+
+  const resumeUrl = 'src/data/Cillo-Michael Resume.pdf';
+
   return (
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hello, My name is Michael</h1>
         <p className={styles.description}>
-          I am a full-stack developer that loves to make fun and exciting apps. Contact me if you would like to learn more!</p>
-        <a href="mailto:michael.cillo@me.com" className={styles.contactBtn}>
-          Contact Me
-        </a>
+          {/* I am a full-stack developer that loves to make fun and exciting apps. Contact me if you would like to learn more! */}
+          although most people just call me Mike. I was raised in a military family before I joined the ARMY after High School. I spent 6 years in the ARMY as a CH-47 Chinook Flight Engineer. I've been deployed to numerous locations around the world. After I got out of the ARMY I earned my bachelors degree in Information Technology. My hobbies include woodworking, photography, and playing video games. I am 42 years old and currently work in the Beverage Industry at Augusta Liquors.
+        </p>
+        <div className={styles.heroButtons}>
+          <a href="mailto:michael.cillo@me.com" className={styles.contactBtn}>
+            Contact Me
+          </a>
+          <a href={resumeUrl} download='Cillo-Michael resumeUrl.pdf' className={styles.resumeBtn}>
+            My Resume
+          </a>
+        </div>
       </div>
       <img
         src={getImageUrl('hero/mcillocartoonportrait.jpeg')}
-        alt='hero image of me'
+        alt='hero cartoon image of me'
         className={styles.heroImg} />
       <div className={styles.topBlur} />
       <div className={styles.bottomBlur} />
