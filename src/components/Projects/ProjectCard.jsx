7a0ebcc2fe520e9a37f98e26cfd06387df13af54
Michael Cillo <117599499+MCillo@users.noreply.github.com>
2024-02-26 19:37:33 -0500

    Update ProjectCard.jsx
    updated project card

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -6,33 +6,28 @@ import styles from "./ProjectCard.module.css";
 import { getImageUrl } from "../../utils";
 
 export const ProjectCard = ({
-  project: { title, imageSrc, description, skills, demo, source },
+  project: { title, imageSrc, description, demo, source },
 }) => {
+
   return (
     <div className={styles.container}>
-      <img
-        src={getImageUrl(imageSrc)}
-        alt={`Image of ${title}`}
-        className={styles.image}
-      />
-      <h3 className={styles.title}>{title}</h3>
-      <p className={styles.description}>{description}</p>
-      <ul className={styles.skills}>
-        {skills.map((skill, id) => {
-          return (
-            <li key={id} className={styles.skill}>
-              {skill}
-            </li>
-          );
-        })}
-      </ul>
-      <div className={styles.links}>
-        <a href={demo} className={styles.link}>
-          Demo
-        </a>
-        <a href={source} className={styles.link}>
-          Source
+      <div className={styles.content}>
+        <img
+          src={getImageUrl(imageSrc)}
+          alt={`Image of ${title}`}
+          className={styles.image}
+        />
+        <h3 className={styles.title}>{title}</h3>
+        <p className={styles.description}>{description}</p>
+
+        <div className={styles.links}>
+          <a href={demo} className={styles.link} target="blank" rel="noopener">
+            Demo
           </a>
+          <a href={source} className={styles.link} target="blank" rel="noopener">
+            Source
+          </a>
+        </div>
       </div>
     </div>
   );
