8ac450f84fab9362e24404eebe3cc660a9c50291
Michael Cillo <117599499+MCillo@users.noreply.github.com>
2024-02-28 15:21:42 -0500

    Update ProjectCard.jsx
    added modal to project card

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,13 +1,25 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState } from "react";
 import { PropTypes } from 'prop-types';
 
 import styles from "./ProjectCard.module.css";
 import { getImageUrl } from "../../utils";
 
+import ProjectModal from "../Modal/Modal";
+
 export const ProjectCard = ({
   project: { title, imageSrc, description, demo, source },
 }) => {
+
+  let [isModalOpen, setModalOpen] = useState(false);
+
+  let openModal = () => {
+    setModalOpen = (true);
+  };
+
+  let closeModal = () => {
+    setModalOpen = (false);
+  };
 
   return (
     <div className={styles.container}>
@@ -20,6 +32,11 @@ export const ProjectCard = ({
         <h3 className={styles.title}>{title}</h3>
         <p className={styles.description}>{description}</p>
 
+        <div className={styles.links}>
+          <button onClick={openModal} className={styles.link}>
+            More Info
+          </button>
+        </div>
         <div className={styles.links}>
           <a href={demo} className={styles.link} target="blank" rel="noopener">
             Demo
