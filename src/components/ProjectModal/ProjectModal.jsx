10d94b428ac9d73c9a62c8a3050ad57304aa9c0b
Michael Cillo <117599499+MCillo@users.noreply.github.com>
2024-03-03 15:37:40 -0500

    Update ProjectModal.jsx
    updated ProjectModal

--- src/components/ProjectModal/ProjectModal.jsx
+++ src/components/ProjectModal/ProjectModal.jsx
@@ -1,29 +1,44 @@
-/* eslint-disable no-unused-vars */
-import React, { useState } from "react";
-import PropTypes from "prop-types";
+// import * as React from 'react';
+// import Box from '@mui/material/Box';
+// import Button from '@mui/material/Button';
+// import Typography from '@mui/material/Typography';
+// import Modal from '@mui/material/Modal';
 
-import styles from "./ProjectModal.module.css";
+// const style = {
+//   position: 'absolute',
+//   top: '50%',
+//   left: '50%',
+//   transform: 'translate(-50%, -50%)',
+//   width: 400,
+//   bgcolor: 'background.paper',
+//   border: '2px solid #000',
+//   boxShadow: 24,
+//   p: 4,
+// };
 
-const ProjectModal = ({ project, onClose }) => {
-  console.log('Rendering ProjectModal');
-  return (
-    <div className={styles.modal} onClick={onClose}>
-      <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
-        <span className={styles.closeButton} onClick={onClose}>
-          &times;
-        </span>
-        <h2 className={styles.modalTitle}>{project.title}</h2>
-        <p className={styles.modalDescription}>{project.description}</p>
-        {/* Add other details you want to display */}
+// export default function ProjectModal() {
+//   const [open, setOpen] = React.useState(false);
+//   const handleOpen = () => setOpen(true);
+//   const handleClose = () => setOpen(false);
 
-      </div>
-    </div>
-  );
-};
-
-ProjectModal.propTypes = {
-  project: PropTypes.object.isRequired,
-  onClose: PropTypes.func.isRequired,
-};
-
-export default ProjectModal;
+//   return (
+//     <div>
+//       <Button onClick={handleOpen}>Open modal</Button>
+//       <Modal
+//         open={open}
+//         onClose={handleClose}
+//         aria-labelledby="modal-modal-title"
+//         aria-describedby="modal-modal-description"
+//       >
+//         <Box sx={style}>
+//           <Typography id="modal-modal-title" variant="h6" component="h2">
+//             Text in a modal
+//           </Typography>
+//           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+//             Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+//           </Typography>
+//         </Box>
+//       </Modal>
+//     </div>
+//   );
+// }
