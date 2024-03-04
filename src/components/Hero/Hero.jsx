2519c05f57931483e64983625ff91368a723d64b
MCillo <117599499+MCillo@users.noreply.github.com>
2024-03-03 22:08:40 -0500

    Update Hero.jsx
    updated font sizes and spacing

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -15,9 +15,9 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hello, my name is Michael</h1>
         <p className={styles.description}>
-          and I'm embarking on an exciting journey into the ever-evolving realm of full-stack web development. As an entry-level web developer, I bring a unique blend of experiences, combining my proficiency in retail and sales with the discipline and teamwork cultivated during my time in the military.
-          <br />
-          My career shift is driven by a passion for technology and problem-solving. While my previous roles honed my detail-oriented nature and communication skills through customer interactions and public speaking, my military service equipped me with a commitment to accountability and achieving excellence beyond standards.
+          And I'm embarking on an exciting journey into the ever-evolving realm of full-stack web development. As an entry-level web developer, I bring a unique blend of experiences, combining my proficiency in retail and sales with the discipline and teamwork cultivated during my time in the military.
+          <br /><br />
+         <p> My career shift is driven by a passion for technology and problem-solving. While my previous roles honed my detail-oriented nature and communication skills through customer interactions and public speaking, my military service equipped me with a commitment to accountability and achieving excellence beyond standards.</p>
           <br />
           I'm thrilled to channel my diverse background into creating innovative web solutions. Join me on this transformative journey as I embrace new challenges and contribute my skills to the dynamic field of web development!
         </p>
