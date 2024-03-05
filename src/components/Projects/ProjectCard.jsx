21a0772718b877b525b1f052ba0385cc293ff48d
MCillo <117599499+MCillo@users.noreply.github.com>
2024-03-04 19:58:54 -0500

    Update ProjectCard.jsx
    removed unused code

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -6,15 +6,10 @@ import PropTypes from 'prop-types';
 import styles from "./ProjectCard.module.css";
 import { getImageUrl } from "../../utils";
 
-// Start Modal Code
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-// End Modal Code
 
 export const ProjectCard = ({
-  project: { title, imageSrc, description, skills, demo, source, caseStudy, overview, purposeContext, objective, approach, challenges, duration, credits },
+  project: { title, imageSrc, description, skills, demo, caseStudy },
 }) => {
 
   const [open, setOpen] = useState(false);
@@ -36,46 +31,13 @@ export const ProjectCard = ({
           <a href={demo} className={styles.link} target="blank" rel="noopener">
             Demo
           </a>
-          {/* <a href={source} className={styles.link} target="blank" rel="noopener">
-            Source
-          </a> */}
-        </div>
-        {/* Start Modal Code */}
-        <div className={styles.modalLinks}>
-          <a className={styles.modalLink} onClick={handleOpen} >More Info!</a>
+          <a className={styles.link} onClick={handleOpen} >More Info!</a>
+
           <Modal
             open={open}
             onClose={handleClose}
-          // className={styles.modal}
-          // aria-labelledby="modal-modal-title"
-          // aria-describedby="modal-modal-description"
           >
             <div className={styles.modal} onClick={handleClose}>
-              {/* <h1 className={styles.caseStudyTitle}>{caseStudy}</h1>
-              <h3 className={styles.caseStudySection}>Overview</h3>
-              <p className={styles.caseStudyDescription}>{overview}</p>
-              <h3 className={styles.caseStudySection}>Purpose & Context</h3>
-              <p className={styles.caseStudyDescription}>{purposeContext}</p>
-              <h3 className={styles.caseStudySection}>Objective</h3>
-              <p className={styles.caseStudyDescription}>{objective}</p>
-              <h3 className={styles.caseStudySection}>Approach</h3>
-              <h5 className={styles.caseStudySkills}>{skills}</h5>
-              {/* <p className={styles.caseStudyDescription}>{approach}</p>
-              <h3 className={styles.caseStudySection}>Challenges</h3>
-              <p className={styles.caseStudyDescription}>{challenges}</p>
-              <h3 className={styles.caseStudySection}>Duration</h3>
-              <p className={styles.caseStudyDescription}>{duration}</p>
-              <h3 className={styles.caseStudySection}>Credits</h3>
-              <p className={styles.caseStudyDescription}>{credits}</p>
-              <ul className={styles.skills}>
-                {skills.map((skill, id) => {
-                  return (
-                    <li key={id} className={styles.skill}>
-                      {skill}
-                    </li>
-                  );
-                })}
-              </ul> */}
               {/* image of case study */}
               <img className={styles.caseStudy} src={getImageUrl(caseStudy)} alt="" />
 
